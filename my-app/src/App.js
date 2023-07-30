946582bef5a31906169ca0a47ab4fb53985c5cf3
guy1309 <guy132000@gmail.com>
2023-07-31 01:13:38 +0300

    create app and navvbar

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>זהו דף ראשי</h1>
+      {/* כאן אתה יכול להוסיף תוכן נוסף לדף הראשי */}
+    </div>
+  );
+};
+
+export default App;
---
+++ my-app/src/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li><a href="/">דף הבית</a></li>
+        <li><a href="/about">אודות</a></li>
+        <li><a href="/services">שירותים</a></li>
+        <li><a href="/contact">צור קשר</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
