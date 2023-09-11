6fc646eac3aa88d2dd603ef2aadc591b6cdd2fd7
guy1309 <guy132000@gmail.com>
2023-09-12 02:49:22 +0300

    .
    create react app

---
+++ my-app/src/components/RouterPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {  BrowserRouter} from 'react-router-dom'
+
+export default function RouterPage()
+{
+    return(
+        <div>Router</div>
+    )    
+}
---
+++ my-app/src/components/users/Profile.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function profile()
+{
+    return(
+        <div>Profile</div>
+    )    
+}
